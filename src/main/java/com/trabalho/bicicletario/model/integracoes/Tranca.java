eeaba363d5c6216288bd18141c77698977546978
Thaissa Lopes <117691038+thatalps@users.noreply.github.com>
2025-01-23 03:38:52 -0300

    adição de integração com tranca

--- src/main/java/com/trabalho/bicicletario/model/integracoes/Tranca.java
+++ src/main/java/com/trabalho/bicicletario/model/integracoes/Tranca.java
@@ -1,36 +1,106 @@
 package com.trabalho.bicicletario.model.integracoes;
 
-import com.trabalho.bicicletario.model.Bicicleta;
-import lombok.NoArgsConstructor;
+import com.trabalho.bicicletario.model.integracoes.dtos.BicicletaDTO;
+import com.trabalho.bicicletario.model.integracoes.dtos.responses.BicicletaResponse;
+import com.trabalho.bicicletario.model.integracoes.dtos.responses.TrancaResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-@NoArgsConstructor
+@AllArgsConstructor
 public class Tranca {
-    public int idTranca;
-    public int idBicicleta;
-    public String status;
 
-    public Tranca getTranca(int idTranca){
-        this.idTranca = idTranca;
-        return new Tranca();
+    @Qualifier("restTemplate") private final RestTemplate restTemplate;
+    private final String url = "https://bicicletario-4u7u.onrender.com/";
+
+    public TrancaResponse getTranca(int idTranca){
+        //busca tranca no equipamento
+        //cria url
+        String urlReq = url+ "tranca/"+ idTranca;
+        TrancaResponse trancaResponse=null;
+        try {
+            //requisicao
+            ResponseEntity<TrancaResponse> response = restTemplate.getForEntity(urlReq, TrancaResponse.class);
+            //retorno nao é bem sucessido
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                return trancaResponse;//retorna null
+            }
+            trancaResponse = response.getBody();//retorno bem sucedido
+        } catch (Exception ex) {
+            return trancaResponse;//null
+        }
+
+        return trancaResponse;
     }
 
-    public Tranca destrancar(int idTranca, int idBicicleta){
-        this.idTranca = idTranca;
-        this.idBicicleta = idBicicleta;
-        return new Tranca();
+    public TrancaResponse destrancar(int idTranca, int idBicicleta) {
+        //destranca a bicicleta da tranca no equipamento
+
+        //cria url
+        String urlReq = url + "tranca/" + idTranca+ "/destrancar";
+        //monta variavel pro post
+        BicicletaDTO bicicletaDTO = new BicicletaDTO(idBicicleta);
+        //variavel de resposta
+        TrancaResponse trancaResponse = null;
+        try {
+            //requisicao
+            ResponseEntity<TrancaResponse> response = restTemplate.postForEntity(urlReq, bicicletaDTO, TrancaResponse.class);
+            //retorno nao é bem sucessido
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                return trancaResponse;//retorna null
+            }
+            trancaResponse = response.getBody();//retorno bem sucedido
+        } catch (Exception ex) {
+            return trancaResponse;//null
+        }
+
+        return trancaResponse;
     }
 
-    public Bicicleta getBicicletaByIdTranca(int idTranca){
-        Bicicleta bicicleta = new Bicicleta();
-        bicicleta.setId(idTranca);
-        return bicicleta;
+    public BicicletaResponse getBicicletaByIdTranca(int idTranca){
+        //busca bicicleta pela tranca no equipamento
+
+        //cria url
+        String urlReq = url+ "tranca/"+ idTranca + "/bicicleta";
+
+        BicicletaResponse bicicletaResponse=null;
+        try {
+            //requisicao
+            ResponseEntity<BicicletaResponse> response = restTemplate.getForEntity(urlReq, BicicletaResponse.class);
+            //retorno nao é bem sucessido
+            if (!response.getStatusCode().is2xxSuccessful()) {
+//                return trancaResponse;//retorna null
+            }
+            bicicletaResponse = response.getBody();//retorno bem sucedido
+        } catch (Exception ex) {
+            return bicicletaResponse;//null
+        }
+
+       return bicicletaResponse;
     }
 
-    public Bicicleta alterarStatusTranca(String status, int idTranca){
-        this.status = status;
-        this.idTranca = idTranca;
-        return new Bicicleta();
+    public TrancaResponse alterarStatusTranca(String status, int idTranca){
+        //altera status tranca no equipamento
+
+        //cria url
+        String urlReq = url + "tranca/" + idTranca+ "/status/" + status;
+        //variavel de resposta
+        TrancaResponse trancaResponse = null;
+        try {
+            //requisicao
+            ResponseEntity<TrancaResponse> response = restTemplate.postForEntity(urlReq,"", TrancaResponse.class);
+            //retorno nao é bem sucessido
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                return trancaResponse;//retorna null
+            }
+            trancaResponse = response.getBody();//retorno bem sucedido
+        } catch (Exception ex) {
+            return trancaResponse;//null
+        }
+
+        return trancaResponse;
     }
 }
